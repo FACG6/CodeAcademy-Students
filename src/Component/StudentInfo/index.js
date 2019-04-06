b32e9ae0c9cd7542e1bd3cffcdb4bfede1c6ae9b
Jamalat <jamalatshamallakh@gmail.com>
2019-04-06 16:05:00 +0300

    Students

---
+++ src/Component/StudentInfo/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+class StudentInfo extends React.Component{
+    render(){
+        return(
+            <div>dddddddddd</div>
+        )
+    }
+}
+export default StudentInfo;
--- src/Component/Students/index.js
+++ src/Component/Students/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-
+import Student  from '../Student/index'
 class Students extends React.Component {
-    state={
-        users:[]
+    state = {
+        users: [],
     }
-    const users =["AbdallahAmmar96","AhmedAl-Almi","alaabadra","aminalakhsham","Angham116", "Anies12","AymanAlqoqa","denaHS","engshorouq","Fatmasiam","IsraaSulaiman","Jamalat-shamallakh","KhaderMurtaja", "mohammedmh", "naremanhilles", "ahmedisam99"]
-    render(){
-        return(
-                <div className='container'>
-                    <div>Students.......</div>
-                </div>
+    componentDidMount() {
+        fetch('https://api.github.com/orgs/facg6/members?access_token=3ef9f57c5e5505ba2bc016fb7c6ff4c08dd5d1fc').then(res => res.json()).then(res => {
+            const users = ["AbdallahAmmar96", "AhmedAl-Almi", "alaabadra", "aminalakhsham", "Angham116", "Anies12", "AymanAlqoqa", "denaHS", "engshorouq", "Fatmasiam", "IsraaSulaiman", "Jamalat-shamallakh", "KhaderMurtaja", "mohammedmh", "naremanhilles", "ahmedisam99"]
+            if (res) {
+                const result = res.filter((x) => {
+                    for (let i = 0; i < users.length; i++) {
+                        if (x.login === users[i]) {
+                            return true;
+                        }
+                    }
+                })
+                this.setState({ users: result })
+            }
+        })
+    }
+    display = () => {
+        console.log(98787979, this.state.users)
+    }
+    render() {
+    
+        return (
+            <div>
+            {this.state.users.map(user=><Student key={user.login} {...user} />)}
+            <button onClick={this.display}>fff</button>
+            </div>
+
         )
     }
-
 }
 export default Students;
