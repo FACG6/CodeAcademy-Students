10208b670f650735bd04d688c4864545e7416cfd
Jamalat <jamalatshamallakh@gmail.com>
2019-04-08 11:33:03 +0300

    git for student

--- src/Component/StudentInfo/index.js
+++ src/Component/StudentInfo/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import getStudents from '../helpers/getStudent';
-
+require('dotenv').config();
 class StudentInfo extends React.Component {
   state = {
+    name:'',
+    avatar_url:'',
     followers: 0,
     following: 0,
     organizations:'',
+    repos_No :0 ,
+    repo_url:[]
   }
 
   getstdnt = (x) => {
@@ -19,13 +23,28 @@ class StudentInfo extends React.Component {
     if (this.props.userInfo[0]) {
       const url = 'https://api.github.com/users/';
       const info = this.props.userInfo[0];
-      this.getstdnt(`${url}${info.login}`).then(res => {
-        this.setState({ followers: res.followers })
+      
+      this.getstdnt(`${url}${info.login}?access_token=${process.env.REACT_APP_TOKEN }`).then(res => {
+        this.setState({ 
+          followers: res.followers ,
+          name:res.name,
+          following:res.following,
+          avatar_url:res.avatar_url
+
+         })
       })
-      this.getstdnt(`${url}${info.login}`).then(res => {
-        this.setState({ following: res.following })
+      this.getstdnt(`${url}${info.login}/repos?access_token=${process.env.REACT_APP_TOKEN }`).then(res => {
+        let repos_url =[];
+        res.forEach(repo=>{
+          repos_url.push(repo.html_url)
+        })
+        this.setState({ 
+          repos_No :repos_url.length,
+          repo_url:repos_url
+         })
       })
-      this.getstdnt(`${url}${info.login}/orgs`).then(res => {
+    
+      this.getstdnt(`${url}${info.login}/orgs/access_token=${process.env.REACT_APP_TOKEN }`).then(res => {
         let string = ':';
         res.forEach( x => {
           string += `, ${x.login}`;
@@ -36,15 +55,16 @@ class StudentInfo extends React.Component {
   }
 
   render() {
-    if (this.props.userInfo) {
-      const { login, avatar_url, followers_url, following_url, organizations_url, repos_url, subscriptions_url, url } = this.props.userInfo;
+    if (this.state.name && this.state.repos_No) {
       return (
         <div>
-          <h2>{login}</h2>
-          <img src={avatar_url} alt='Student avatar' />
+          <h2>{this.state.name}</h2>
+          <img src={this.state.avatar_url} alt='Student avatar' />
           <p>Followers {this.state.followers}</p>
           <p>Following {this.state.following}</p>
           <p>Organizations{this.state.organizations}</p>
+          <p>Repos No. :{this.state.repos_No}</p>
+          
         </div>
       )
     } else {
